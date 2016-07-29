eab294ae0bfcacf301cbf6c6c6c9e9215d653d7a
Xin Yu <scaner@gmail.com>
2016-07-29 23:53:16 +0800

    demo/vis4/p0

---
+++ is/demos/vis4/pkg/index.html_map_ae3477d.js
@@ -0,0 +1,30 @@
+require.resourceMap({
+  "res": {
+    "three/three": {
+      "url": "/is/demos/vis4//node_modules/three/three_aa966c4.js",
+      "type": "js",
+      "pkg": "p0"
+    },
+    "common": {
+      "url": "/is/demos/vis4//js/common_4ead3d2.js",
+      "type": "js",
+      "deps": [
+        "three/three"
+      ]
+    },
+    "apps/1": {
+      "url": "/is/demos/vis4//js/apps/1_3d951b4.js",
+      "type": "js",
+      "deps": [
+        "three/three",
+        "common"
+      ]
+    }
+  },
+  "pkg": {
+    "p0": {
+      "url": "/is/demos/vis4//pkg/third_7bd3321.js",
+      "type": "js"
+    }
+  }
+});
